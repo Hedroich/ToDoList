befce33a5518917eedbaf68a2e080951e25af038
Hedroich <hedroich@gmail.com>
2023-11-08 20:50:18 +0300

    remove apiView

--- task/task_list/views.py
+++ task/task_list/views.py
@@ -4,35 +4,6 @@ from .forms import CustomUserCreationForm, LoginForm
 from .models import CustomUser, Task
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
-from rest_framework.generics import GenericAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.mixins import CreateModelMixin, ListModelMixin
-from rest_framework.response import Response
-from .serializers import TaskSerializers
-
-
-
-class TaskGenericApiView(GenericAPIView, ListModelMixin, CreateModelMixin):
-    serializer_class = TaskSerializers
-    queryset = Task.objects.all()
-
-    def get(self, request: HttpRequest, *args: tuple, **kwargs: dict) -> Response:
-        tasks = Task.objects.filter(user_id=self.request.user.id)
-        serializer = TaskSerializers(tasks, many=True)
-        return Response(serializer.data)
-
-
-    def post(self, request: HttpRequest, *args: tuple, **kwargs: dict) -> Response:
-        return self.create(request, *args, **kwargs)
-
-
-class SingleTaskApiView(RetrieveUpdateDestroyAPIView):
-    serializer_class = TaskSerializers
-    queryset = Task.objects.all()
-
-    def retrieve(self, request: HttpRequest, *args: tuple, **kwargs: dict) -> Response:
-        tasks = Task.objects.filter(user_id=self.request.user.id) & Task.objects.filter(id=kwargs["pk"])
-        serializer = TaskSerializers(tasks, many=True)
-        return Response(serializer.data)
 
 
 def index(request: HttpRequest) -> HttpResponse:
