17d8b4d14b8c2edb1870414adb40c8d57bfa5185
Hedroich <hedroich@gmail.com>
2023-11-08 20:49:20 +0300

    moved to api

--- task/task_list/api/serializers.py
+++ task/task_list/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Task, CustomUser
+from ..models import Task
 
 
 class TaskSerializers(serializers.ModelSerializer):
---
+++ task/task_list/api/views.py
@@ -0,0 +1,29 @@
+from rest_framework.generics import GenericAPIView, RetrieveUpdateDestroyAPIView
+from django.http import HttpRequest
+from rest_framework.mixins import CreateModelMixin, ListModelMixin
+from rest_framework.response import Response
+from ..models import Task
+from ..api.serializers import TaskSerializers
+
+class TaskGenericApiView(GenericAPIView, ListModelMixin, CreateModelMixin):
+    serializer_class = TaskSerializers
+    queryset = Task.objects.all()
+
+    def get(self,  request: HttpRequest, *args: tuple, **kwargs: dict) -> Response:
+        tasks = Task.objects.filter(user_id=self.request.user.id)
+        serializer = TaskSerializers(tasks, many=True)
+        return Response(serializer.data)
+
+
+    def post(self, request: HttpRequest, *args: tuple, **kwargs: dict) -> Response:
+        return self.create(request, *args, **kwargs)
+
+
+class SingleTaskApiView(RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializers
+    queryset = Task.objects.all()
+
+    def retrieve(self, request: HttpRequest, *args: tuple, **kwargs: dict) -> Response:
+        tasks = Task.objects.filter(user_id=self.request.user.id) & Task.objects.filter(id=kwargs["pk"])
+        serializer = TaskSerializers(tasks, many=True)
+        return Response(serializer.data)
